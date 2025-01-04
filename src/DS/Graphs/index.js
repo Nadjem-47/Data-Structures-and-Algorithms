8192de04be96c0e8c0f99b2dfbd3cf9ca6e79203
Nadjem <A.Najemaddin@naseej.com>
2025-01-04 13:29:50 +0100

    has path problem

--- src/DS/Graphs/index.js
+++ src/DS/Graphs/index.js
@@ -29,8 +29,7 @@ graph.addEdge("A", "C")
 graph.addEdge("A", "B")
 graph.addEdge("B", "D")
 graph.addEdge("C", "E")
-graph.addEdge("E", "F")
-
+// graph.addEdge("E", "F")
 
 const depthFirstPrint = (graph, source) => {
   const stack = [source]
@@ -57,3 +56,47 @@ const breadthFirstPrint = (graph, source) => {
 }
 
 breadthFirstPrint(graph.adjacencyList, "A")
+
+const hasPathDfs = (graph, src, dst) => {
+  const stack = [src]
+
+  while (stack.length) {
+    const current = stack.pop()
+
+    if (current === dst) return true
+
+    stack.push(...graph[current])
+  }
+  return false
+}
+
+hasPathDfs(graph.adjacencyList, "A", "F")
+
+const hasPathDfsRecursion = (graph, src, dst) => {
+  if (src === dst) return true
+
+  for (let neighbor of graph[src]) {
+    if (hasPathDfsRecursion(graph, neighbor, dst)) {
+      return true
+    }
+  }
+
+  return false
+}
+
+console.log(hasPathDfsRecursion(graph.adjacencyList, "A", "F"))
+
+const hasPathBfs = (graph, src, dst) => {
+  const queue = [src]
+
+  while (queue.length) {
+    const current = queue.shift()
+
+    if (current === dst) return true
+
+    queue.push(...graph[current])
+  }
+  return false
+}
+
+hasPathBfs(graph.adjacencyList, "A", "E")
