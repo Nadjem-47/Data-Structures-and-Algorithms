04321e8dd8e455f34b51389690c0d94110634236
Nadjem <A.Najemaddin@naseej.com>
2025-02-16 18:15:20 +0100

    Binary search easy

---
+++ src/problems/BinarySearch.js
@@ -0,0 +1,22 @@
+export const search = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        let middleIdx = Math.floor((left + right) / 2);
+        let middle = nums[middleIdx];
+
+        if (middle === target) return middleIdx;
+
+        if (middle > target) {
+            right = middleIdx - 1;
+        } else {
+            left = middleIdx + 1;
+        }
+    }
+
+    return -1; // Target not found
+};
+
+console.log(search([-1,0,3,5,9,12], 9)); // Expected output: 4
+console.log(search([-1,0,3,5,9,12], 2)); // Expected output: -1
