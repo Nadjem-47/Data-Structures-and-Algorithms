23a2567f9c9b51998371bf785021151a75c3d023
Nadjem <A.Najemaddin@naseej.com>
2025-03-10 21:29:10 +0100

    isValid

---
+++ src/problems/isValid.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+
+    if (s.length % 2 !== 0) return false;
+
+  const left = ["[", "(", "{"];
+  const stack = [];
+
+  for (let char of s) {
+      if (left.includes(char)) {
+          stack.push(char);
+      } else {
+          if (!stack.length) return false;
+          
+          const last = stack.pop();
+          
+          if (
+              (last === "[" && char !== "]") ||
+              (last === "(" && char !== ")") ||
+              (last === "{" && char !== "}")
+          ) {
+              return false;
+          }
+      }
+  }
+
+  return !stack.length;
+};
