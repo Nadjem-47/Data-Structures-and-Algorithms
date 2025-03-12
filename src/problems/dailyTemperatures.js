6fd75261673098b6d323b6bbb90a755e0acd0c5d
Nadjem <A.Najemaddin@naseej.com>
2025-03-12 21:11:36 +0100

    dailyTemperatures

---
+++ src/problems/dailyTemperatures.js
@@ -0,0 +1,20 @@
+// todo revisite
+const dailyTemperatures = function(temperatures) {
+    const stack = []
+   const result = Array(temperatures.length).fill(0);
+
+   for (let i = 0; i < temperatures.length; i++) {
+       const temprature = temperatures[i]
+
+       while (stack.length && temprature > temperatures[stack[stack.length - 1]]) {
+           const prevIndex = stack.pop()
+           result[prevIndex] = i - prevIndex
+
+       }
+
+       stack.push(i)
+
+   }
+
+   return result
+};
