2b469e8eedfa57449782dce0248abdc202bff931
Nadjem <A.Najemaddin@naseej.com>
2025-04-10 19:18:21 +0100

    minEatingSpeed

--- src/problems/topKFrequent.js
+++ src/problems/topKFrequent.js
@@ -9,6 +9,18 @@
  * This function uses a frequency map to count occurrences of each number,
  * then utilizes bucket sort to gather numbers by frequency, and finally
  * extracts the top k frequent elements.
+ 
+
+    Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+        Example 1:
+
+        Input: nums = [1,1,1,2,2,3], k = 2
+        Output: [1,2]
+        Example 2:
+
+        Input: nums = [1], k = 1
+        Output: [1]
  */
 
 
