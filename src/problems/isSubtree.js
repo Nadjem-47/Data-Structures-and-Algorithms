98772418cd948397b2f73653a35334d9be15c8ed
Nadjem <A.Najemaddin@naseej.com>
2025-05-02 10:51:16 +0100

    Solve problems

---
+++ src/problems/isSubtree.js
@@ -0,0 +1,38 @@
+/**
+Given the roots of two binary trees root and subRoot, return true if there is a subtree of root with the same structure and node values of subRoot and false otherwise.
+
+A subtree of a binary tree tree is a tree that consists of a node in tree and all of this node's descendants. The tree tree could also be considered as a subtree of itself.
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} subRoot
+ * @return {boolean}
+ */
+var isSubtree = function(root, subRoot) {
+
+    if (!subRoot)
+        return true
+
+    if (!root)
+        return false;
+
+    if (isSameTree(root, subRoot))
+        return true;
+
+    return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot)
+};
+
+var isSameTree = function(p, q) {
+
+    if (!p && !q)
+        return true;
+
+    if (!p || !q)
+        return false;
+
+    if (p.val !== q.val)
+        return false;
+
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+
+};
